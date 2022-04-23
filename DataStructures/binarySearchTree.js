308fec06d98e09c1fc92a5d857e46c6722410a0f
anthgrim <anthgrim@icloud.com>
2022-04-23 16:36:37 -0400

    Adding Binary Search Tree part 2 operations

--- DataStructures/binarySearchTree.js
+++ DataStructures/binarySearchTree.js
@@ -149,4 +149,112 @@ class BST {
     };
     this.root = removeNode(this.root, data);
   }
+
+  isBalanced() {
+    return this.findMinHeight() >= this.findMaxHeight() - 1;
+  }
+
+  findMinHeight(node = this.root) {
+    if (node === null) {
+      return -1;
+    }
+
+    let left = this.findMinHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+
+    if (left < right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+
+  findMaxHeight(node = this.root) {
+    if (node === null) {
+      return -1;
+    }
+
+    let left = this.findMinHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+
+    if (left > right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+
+  inOrder() {
+    if (this.root === null) {
+      return null;
+    }
+
+    let result = new Array();
+
+    function traverseInOrder(node) {
+      node.left && traverseInOrder(node.left);
+      result.push(node.data);
+      node.right && traverseInOrder(node.right);
+    }
+
+    traverseInOrder(this.root);
+    return result;
+  }
+
+  preOrder() {
+    if (this.root === null) {
+      return null;
+    }
+    let result = new Array();
+
+    function traversePreOrder(node) {
+      result.push(node.data);
+      node.left && traversePreOrder(node.left);
+      node.right && traversePreOrder(node.right);
+    }
+
+    traversePreOrder(this.root);
+    return result;
+  }
+
+  postorder() {
+    if (this.root === null) {
+      return null;
+    }
+    let result = newArray();
+
+    function traversePostOrder(node) {
+      node.left && traversePostOrder(node.left);
+      node.right && traversePostOrder(node.right);
+      result.push(node.data);
+    }
+
+    traversePostOrder(this.root);
+    return result;
+  }
+
+  levelOrder() {
+    let result = [];
+    let Q = [];
+
+    if (this.root !== null) {
+      Q.push(this.root);
+
+      while (Q.length > 0) {
+        let node = Q.shift();
+        result.push(node.data);
+
+        if (node.left !== null) {
+          Q.push(node.left);
+        }
+
+        if (node.right !== null) {
+          Q.push(node.right);
+        }
+      }
+      return result;
+    } else {
+      return null;
+    }
+  }
 }
